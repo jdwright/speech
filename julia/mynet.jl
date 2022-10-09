9662adc7d3a3ae3332c7e27b6a01a3684967e0e2
Jonathan Wright <jdwright@ldc.upenn.edu>
2022-10-09 10:18:18 -0400

    minor edits

--- julia/mynet.jl
+++ julia/mynet.jl
@@ -1,21 +1,19 @@
 "missing some piece of this tutorial, can't find on the web"
 
 
-using Flux: onehotbatch, onecold, crossentropy, throttle
+using Flux #: onehotbatch, onecold, crossentropy, throttle
 using Base.Iterators: repeated
-function trainmodel(imgs, labels, labelset)
-    X = hcat(float.(reshape.(imgs, :))...) #stack all the images
-    Y = onehotbatch(labels, labelset) # just a common way to encode categorical variables
+function trainmodel(X, Y, labelset, xy)
     # xy = size(first(imgs))
-    xy = ( length(first(imgs)), 1 )
     m = Chain(
-        Dense(xy[1]*xy[2], 32, relu),
-        Dense(32, length(labelset)),
+        Dense(xy[1]*xy[2], 8, relu),
+        Dense(8, length(labelset)),
         softmax
     )
     loss(x, y) = crossentropy(m(x), y) 
     opt = ADAM(); # popular stochastic gradient descent variant
-    dataset = repeated((X,Y),200) # repeat the data set 200 times, as opposed to @epochs 200 ...
+    n = 200
+    dataset = repeated((X,Y), n) # repeat the data set 200 times, as opposed to @epochs 200 ...
     evalcb = () -> @show(loss(X, Y)) # callback to show loss
     Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10)); #took me ~5 minutes to train on CPU
     m
